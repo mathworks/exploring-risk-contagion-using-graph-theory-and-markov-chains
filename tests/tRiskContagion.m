e6a8306bbbfa7ff837dd19abf343bf880a248a2f
Ken Deeley <kdeeley@mathworks.com>
2026-05-11 14:24:04 +0100

    Updated YAML and added CI filter

--- tests/tRiskContagion.m
+++ tests/tRiskContagion.m
@@ -1,6 +1,18 @@
 classdef tRiskContagion < matlab.unittest.TestCase
     %TRISKCONTAGION Check the main example script.
 
+    methods ( TestClassSetup )
+
+        function filterTestOnCI( testCase )
+
+            ci = getenv( "GITHUB_ACTIONS" ) == "true";
+            testCase.assumeFalse( ci, ...
+                "This test only runs locally, not in CI systems." )
+
+        end % filterTestOnCI
+
+    end % methods ( TestClassSetup )
+
     methods ( Test )
 
         function tScriptIsWarningFree( testCase )
